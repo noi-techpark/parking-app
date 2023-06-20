8151b75dec42c53c8c8ac5e8d7b2fd5ea373bdc5
Simon Dalvai <s.dalvai@noi.bz.it>
2023-06-20 13:54:16 +0200

    fix available slots font size | 3

--- components/ui/generic/AvailableSlotsCard.vue
+++ components/ui/generic/AvailableSlotsCard.vue
@@ -13,7 +13,7 @@ SPDX-License-Identifier: AGPL-3.0-or-later
       red: realTime && (available === 0 || available / total < 0.2),
     }"
   >
-    <span class="count">
+    <span id="count">
       {{ available }}
     </span>
   </div>
@@ -64,8 +64,8 @@ export default {
 </script>
 
 <style lang="postcss">
-& .count {
-  @apply font-bold text-4xl text-base;
+#count {
+  @apply font-bold text-4xl;
 }
 
 .available-slots {
