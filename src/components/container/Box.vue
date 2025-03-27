a3afa2513736c06192a8f9a9b3f25f4b1f852ae5
luho <luca.pasquarelli.villa@gmail.com>
2025-03-27 11:03:08 +0100

    push to prod new tag

--- src/components/container/Box.vue
+++ src/components/container/Box.vue
@@ -37,7 +37,7 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 'use strict'
 export default {
   props: {
-    /** @values default, compactt */
+    /** @values default, compact */
     aspect: {
       type: String,
       default: 'default',
